57b9e25e3ca55ef30032b9d5e4889bce205a47ef
Gabriele Di Maio <gabrydm2006@gmail.com>
2026-06-19 17:01:12 +0200

    Creazione dei Bean Ordine e DettaglioOrdine

---
+++ src/main/java/model/DettaglioOrdine.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.io.Serializable;
+
+public class DettaglioOrdine implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int idOrdine;
+	private int idProdotto;
+	private int quantita;
+	private double prezzoAcquisto;
+	
+	public DettaglioOrdine() {}
+
+	public int getIdOrdine() {
+		return idOrdine;
+	}
+
+	public void setIdOrdine(int idOrdine) {
+		this.idOrdine = idOrdine;
+	}
+
+	public int getIdProdotto() {
+		return idProdotto;
+	}
+
+	public void setIdProdotto(int idProdotto) {
+		this.idProdotto = idProdotto;
+	}
+
+	public int getQuantita() {
+		return quantita;
+	}
+
+	public void setQuantita(int quantita) {
+		this.quantita = quantita;
+	}
+
+	public double getPrezzoAcquisto() {
+		return prezzoAcquisto;
+	}
+
+	public void setPrezzoAcquisto(double prezzoAcquisto) {
+		this.prezzoAcquisto = prezzoAcquisto;
+	}
+
+}
---
+++ src/main/java/model/Ordine.java
@@ -0,0 +1,139 @@
+package model;
+
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ordine implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int idOrdine;
+	private int idUtente;
+	private Date data;
+	private double totale;
+	private String metodoPagamento;
+	private String idTransazione;
+	private String nome;
+	private String cognome;
+	private String telefono;
+	private String viaNum;
+	private int cap;
+	private String provincia;
+	
+	private List<DettaglioOrdine> dettagli;
+	
+	public Ordine() {
+		this.dettagli = new ArrayList<DettaglioOrdine>();
+	}
+
+	public int getIdOrdine() {
+		return idOrdine;
+	}
+
+	public void setIdOrdine(int idOrdine) {
+		this.idOrdine = idOrdine;
+	}
+
+	public int getIdUtente() {
+		return idUtente;
+	}
+
+	public void setIdUtente(int idUtente) {
+		this.idUtente = idUtente;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public double getTotale() {
+		return totale;
+	}
+
+	public void setTotale(double totale) {
+		this.totale = totale;
+	}
+
+	public String getMetodoPagamento() {
+		return metodoPagamento;
+	}
+
+	public void setMetodoPagamento(String metodoPagamento) {
+		this.metodoPagamento = metodoPagamento;
+	}
+
+	public String getIdTransazione() {
+		return idTransazione;
+	}
+
+	public void setIdTransazione(String idTransazione) {
+		this.idTransazione = idTransazione;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getViaNum() {
+		return viaNum;
+	}
+
+	public void setViaNum(String viaNum) {
+		this.viaNum = viaNum;
+	}
+
+	public int getCap() {
+		return cap;
+	}
+
+	public void setCap(int cap) {
+		this.cap = cap;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public List<DettaglioOrdine> getDettagli() {
+		return dettagli;
+	}
+
+	public void setDettagli(List<DettaglioOrdine> dettagli) {
+		this.dettagli = dettagli;
+	}
+	
+	public void addDettaglio(DettaglioOrdine dettaglio) {
+		this.dettagli.add(dettaglio);
+	}
+}
