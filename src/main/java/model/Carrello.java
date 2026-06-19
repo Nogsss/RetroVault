ffcfe24f76abbc8ed9a54d61c7ae216d6b92dca4
Gabriele Di Maio <gabrydm2006@gmail.com>
2026-06-19 15:43:00 +0200

    Aggiunto il metodo aggiungiProdotto a Carrello.java

--- src/main/java/model/Carrello.java
+++ src/main/java/model/Carrello.java
@@ -22,4 +22,24 @@ public class Carrello implements Serializable{
 	public void setProdotti(List<ElementoCarrello> prodotti) {
 		this.prodotti = prodotti;
 	}
+	
+	//metodi di gestione carrello
+		public void aggiungiProdotto(Prodotto prodotto, int quantità) {
+			
+			//Se la quantità non è valida non fare nulla
+			if(quantità <= 0) return;
+			
+			//Controllo se il prodotto è già nel carrello
+			for(ElementoCarrello elem : prodotti) {
+				if(elem.getProdotto().getId() == prodotto.getId()) {
+					elem.setQuantità(elem.getQuantità()+quantità);
+					return;
+				}
+			}	//Se esce dal for il prodotto non era già nel carrello e lo aggiungo
+			
+			ElementoCarrello el = new ElementoCarrello();
+			el.setProdotto(prodotto);
+			el.setQuantità(quantità);
+			prodotti.add(el);
+		}
 }
