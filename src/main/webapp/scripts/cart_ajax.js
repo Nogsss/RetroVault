8f449466abae24668d83ec616fdae626a6be13bf
Gabriele Di Maio <gabrydm2006@gmail.com>
2026-06-23 15:35:30 +0200

    Creato il codice javascript per la chiamata ajax per aggiungere prodotti al carrello

---
+++ src/main/webapp/scripts/cart_ajax.js
@@ -0,0 +1,48 @@
+function aggiungiAlCarrello(idProdotto) {
+    var request = createXMLHttpRequest();
+    //callback
+    if(request){
+		request.onreadystatechange = function() {
+        	if (this.readyState == 4 && this.status == 200) {
+            	var response = JSON.parse(this.responseText);
+            	document.getElementById("cart-counter").innerHTML = response.nuovoTotale;
+            	var feedback = document.getElementById("feedback-msg");
+            	feedback.innerHTML = "Prodotto aggiunto al carrello!";
+        	}
+		}
+	};
+	
+	setTimeout(function() { 
+		feedback.innerHTML = "";
+		if(request.readyState < 4){
+			request.abort();
+		}
+	}, 15000);
+ 	
+    var url ="RetroVault/carrello?action=addAjax&id=" + idProdotto;
+    
+    request.open("GET", url, true);
+    request.send();
+}
+
+function createXMLHttpRequest() {
+	var request;
+	try{
+		request = new XMLHttpRequest();
+	}
+	catch(e) {
+		try{
+			request = new ActiveXObject("Msxm12.XMLHTTP");
+		}
+		catch(e) {
+			try{
+				request = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			catch(e){
+				alert("Il browser non supporta AJAX");
+				return null;
+			}
+		}
+	}
+	return request;
+}
